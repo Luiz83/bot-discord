c4cac1aabda31db357553ac0b9f380def05a5065
Luiz Pachioni <lopachioni@gmail.com>
2023-09-05 09:35:43 -0300

    ajustando versão do node e dependências do projeto

--- src/callMonitor.js
+++ src/callMonitor.js
@@ -1,4 +1,4 @@
-import { Client, GatewayIntentBits, EmbedBuilder } from 'discord.js';
+import { Client, GatewayIntentBits, EmbedBuilder } from "discord.js";
 import dotenv from 'dotenv';
 
 dotenv.config();
