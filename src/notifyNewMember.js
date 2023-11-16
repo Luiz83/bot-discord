0ec39cefd73dac72c79ed935645edf9fc0327605
Luiz Pachioni <lopachioni@gmail.com>
2023-11-16 12:19:30 -0300

    fix/ user cannot receive dm

--- src/notifyNewMember.js
+++ src/notifyNewMember.js
@@ -16,12 +16,21 @@ export async function NotifyNewMember() {
 
         client.on('guildMemberAdd', async (member) => {
 
+            if (member.bot) return;
+
             const Embed = new EmbedBuilder()
                 .setColor(0xfd3c23)
                 .setTitle("Bem-vindo(a)!")
                 .setDescription("Não se esqueça de reagir a mensagem no canal #acesso para poder visualizar todo o conteúdo do servidor!")
-            
-                await member.send({ embeds: [Embed] });
+
+            let response = await member.send({ embeds: [Embed] })
+                .then(() => console.log(`Mensagem enviada para ${member.user.tag}`))
+                .catch(() => {
+                    console.log(`O ${member.user.tag} não pode receber DM`)
+                    return false;
+                })
+
+            if (!response) return;
         });
 
         client.login(TOKEN);
